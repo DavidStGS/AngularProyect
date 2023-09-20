b2feb7b1b519d0fd285831e2ebf6b2c18e88b352
DavidSGS <davidgarcia200124@gmail.com>
2023-09-20 01:43:09 -0500

    Leccion 2 Mejora en la validacion

--- src/app/form-destino-vieajes/form-destino-vieajes.component.ts
+++ src/app/form-destino-vieajes/form-destino-vieajes.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DestinoViajes } from '../model/destino-viajes.model';
 import { fromEvent } from 'rxjs';
-import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { map, filter, debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
 import { ajax, AjaxResponse } from 'rxjs/ajax';
 
 
@@ -16,35 +16,46 @@ export class FormDestinoVieajesComponent implements OnInit {
 
   searchResults: string[] = [];
 
+  campoNombreVacio: boolean = true; // Inicializamos como true
+
+  get nombreControl(): AbstractControl | null {
+    return this.fb.get('nombre');
+  }
+
   ngOnInit() {
     const elemNombre = <HTMLInputElement>document.getElementById('nombre');
     fromEvent(elemNombre, 'input')
       .pipe(
         map((e: Event) => (e.target as HTMLInputElement).value),
-        filter(text => (text as string).length > 4),
         debounceTime(200),
         distinctUntilChanged(),
+        tap((text: string) => {
+          this.campoNombreVacio = text.trim() === ''; // Verificamos si el campo está vacío
+        }),
+        filter(text => text.length > 4),
         switchMap(() => ajax<string[]>('/assets/datos.json'))
       )
       .subscribe((ajaxResponse: AjaxResponse<string[]>) => {
         this.searchResults = ajaxResponse.response;
       });
   }
+
   @Output() onItemAdded: EventEmitter<DestinoViajes>;
   fb: FormGroup;
+
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
     this.fb = fb.group({
-      nombre: ['', Validators.required],
+      nombre: ['', [Validators.required, Validators.minLength(4)]],
       url: ['']
+
     });
 
     this.fb.valueChanges.subscribe((form: any) => {
       console.log('cambio el formulario: ', form);
     });
   }
-
 
   guardar(nombre: string, url: string): boolean {
     let d = new DestinoViajes(nombre, url);
