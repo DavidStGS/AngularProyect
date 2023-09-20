8075dbc0ef964d5ebe1b010dd4978d5fa9a0e19f
DavidSGS <davidgarcia200124@gmail.com>
2023-09-19 20:18:23 -0500

    Leccion3 mensaje de actividad 2

--- src/app/lista-viajes/lista-viajes.component.ts
+++ src/app/lista-viajes/lista-viajes.component.ts
@@ -12,8 +12,8 @@ export class ListaViajesComponent implements OnInit {
 
   @Output() onItemAdded: EventEmitter<DestinoViajes>;
   destinos: DestinoViajes[];
-
   ultimaSuscripcion: string;
+
 
   constructor(public DestinoApiModel: DestinoApiModel) {
     this.onItemAdded = new EventEmitter();
@@ -47,3 +47,57 @@ export class ListaViajesComponent implements OnInit {
     this.DestinoApiModel.suscribirseAViaje(viaje);
   }
 }
+
+/* ESTA ES LA OPCION DONDE GUARDA TODAS LAS SUSCRIPCIONES EN UN ARRAY Y LAS MUESTRA EN EL HTML
+import { Component, EventEmitter, OnInit, Output, } from '@angular/core';
+import { DestinoViajes } from '../model/destino-viajes.model';
+import { DestinoApiModel } from '../model/destino-api.model';
+
+@Component({
+  selector: 'app-lista-viajes',
+  templateUrl: './lista-viajes.component.html',
+  styleUrls: ['./lista-viajes.component.css'],
+})
+
+export class ListaViajesComponent implements OnInit {
+
+  @Output() onItemAdded: EventEmitter<DestinoViajes>;
+
+  destinos: DestinoViajes[];
+
+  ultimaSuscripcion: string[] = [];
+
+
+  constructor(public DestinoApiModel: DestinoApiModel) {
+    this.onItemAdded = new EventEmitter();
+    this.destinos = [];
+    this.ultimaSuscripcion.push();
+  }
+
+  ngOnInit() {
+    this.destinos = this.DestinoApiModel.getAll();
+
+    this.DestinoApiModel.getPreferidoObservable().subscribe(destino => {
+      if (destino) {
+        const nombreDestino = destino.nombre;
+        this.ultimaSuscripcion.push(`Preferiste el destino: ${nombreDestino}`);
+        console.log(this.ultimaSuscripcion);
+      }
+    });
+  }
+  agregado(d: DestinoViajes) {
+    this.DestinoApiModel.add(d);
+    this.onItemAdded.emit(d);
+  }
+
+  elegido(e: DestinoViajes) {
+    this.DestinoApiModel.getAll().forEach(x => x.setSelected(false));
+    e.setSelected(true);
+    const nombreDestino = e.nombre;
+    this.ultimaSuscripcion.push(`Preferiste el destino: ${nombreDestino}`);
+  }
+
+  suscribirseALaNotificacion(viaje: string) {
+    this.DestinoApiModel.suscribirseAViaje(viaje);
+  }
+} */
